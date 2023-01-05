fb087eb3915332d79c0a33a235c96acb83c3c473
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-01-05 15:59:03 -0600

    fix rank column

--- R/fct_tables.R
+++ R/fct_tables.R
@@ -30,17 +30,19 @@ rankings_table <- function(input_df, input_position, input_player, input_rankers
                                                         .data$Pos == "RB" ~ round(rowMeans(select(filter(input_df, .data$Pos %in% input_position), all_of(input_rankers)), na.rm = TRUE) * 1.03, 3),
                                                         .data$Pos == "WR" ~ round(rowMeans(select(filter(input_df, .data$Pos %in% input_position), all_of(input_rankers)), na.rm = TRUE) * 1.00, 3),
                                                         .data$Pos == "TE" ~ round(rowMeans(select(filter(input_df, .data$Pos %in% input_position), all_of(input_rankers)), na.rm = TRUE) * 0.96, 3))) |>
-          dplyr::arrange(dplyr::desc(.data$`ADJ Grade`))
+          dplyr::arrange(dplyr::desc(.data$`ADJ Grade`)) |>
+          dplyr::mutate(Rank = 1:dplyr::n())
       } else {
         df <- input_df |>
-          dplyr::filter(.data$Player %in% input_player) |>
           dplyr::select(c(.data$Player:.data$`AVG Grade`), all_of(input_rankers)) |>
-          dplyr::mutate(`AVG Grade` = round(rowMeans(select(filter(input_df, .data$Player %in% input_player), all_of(input_rankers)), na.rm = TRUE), 3),
-                        `ADJ Grade` =  dplyr::case_when(.data$Pos == "QB" ~ round(rowMeans(select(filter(input_df, .data$Player %in% input_player), all_of(input_rankers)), na.rm = TRUE) * 1.06, 3),
-                                                        .data$Pos == "RB" ~ round(rowMeans(select(filter(input_df, .data$Player %in% input_player), all_of(input_rankers)), na.rm = TRUE) * 1.03, 3),
-                                                        .data$Pos == "WR" ~ round(rowMeans(select(filter(input_df, .data$Player %in% input_player), all_of(input_rankers)), na.rm = TRUE) * 1.00, 3),
-                                                        .data$Pos == "TE" ~ round(rowMeans(select(filter(input_df, .data$Player %in% input_player), all_of(input_rankers)), na.rm = TRUE) * 0.96, 3))) |>
-          dplyr::arrange(dplyr::desc(.data$`ADJ Grade`))
+          dplyr::mutate(`AVG Grade` = round(rowMeans(select(input_df, all_of(input_rankers)), na.rm = TRUE), 3),
+                        `ADJ Grade` =  dplyr::case_when(.data$Pos == "QB" ~ round(rowMeans(select(input_df, all_of(input_rankers)), na.rm = TRUE) * 1.06, 3),
+                                                        .data$Pos == "RB" ~ round(rowMeans(select(input_df, all_of(input_rankers)), na.rm = TRUE) * 1.03, 3),
+                                                        .data$Pos == "WR" ~ round(rowMeans(select(input_df, all_of(input_rankers)), na.rm = TRUE) * 1.00, 3),
+                                                        .data$Pos == "TE" ~ round(rowMeans(select(input_df, all_of(input_rankers)), na.rm = TRUE) * 0.96, 3))) |>
+          dplyr::arrange(dplyr::desc(.data$`ADJ Grade`)) |>
+          dplyr::mutate(Rank = 1:dplyr::n()) |>
+          dplyr::filter(.data$Player %in% input_player)
       }
     }
 
